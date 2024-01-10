fa1700b31c048fe5278a79e1e5d63dac4fc94026
Gabriele02 <gagag@g,ail.com>
2024-01-10 16:44:59 +0100

    finish work

--- src/main/java/org/learning/la/mia/pizzeria/crud/controller/PizzaController.java
+++ src/main/java/org/learning/la/mia/pizzeria/crud/controller/PizzaController.java
@@ -1,14 +1,14 @@
 package org.learning.la.mia.pizzeria.crud.controller;
 
+import jakarta.validation.Valid;
 import org.learning.la.mia.pizzeria.crud.interfaccie.PizzeriaRepository;
 import org.learning.la.mia.pizzeria.crud.model.Pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -40,5 +40,24 @@ public class PizzaController {
         }
 
     }
+
+    @GetMapping("/newPizza")
+    public String create(Model model) {
+        Pizza pizza = new Pizza();
+        model.addAttribute("pizza", pizza);
+        return "pizzas/newPizza";
+    }
+
+    @PostMapping("/newPizza")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "pizzas/newPizza";
+        } else {
+            Pizza savePizza = pizzeriaRepository.save(formPizza);
+            return "redirect:/pizzas/list/" + savePizza.getName();
+        }
+
+    }
+
 
 }
--- src/main/java/org/learning/la/mia/pizzeria/crud/model/Pizza.java
+++ src/main/java/org/learning/la/mia/pizzeria/crud/model/Pizza.java
@@ -1,18 +1,24 @@
 package org.learning.la.mia.pizzeria.crud.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "pizzas")
 public class Pizza {
     @Id
+    @NotEmpty(message = "Name must not be blank")
+    @Column(nullable = false)
     private String name;
     private String description;
 
     @Lob
     private String photo;
     @Column(nullable = false)
+    @Min(value = 0)
     private BigDecimal price;
 
     public String getName() {
